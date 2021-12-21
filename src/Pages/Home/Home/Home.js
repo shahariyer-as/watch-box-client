7c425a3d3b20c1cb584c4cd4584a9936fe52a922
shahariyer-as <shahariyer.as@gmail.com>
2021-12-21 23:50:50 +0600

    first commit

---
+++ src/Pages/Home/Home/Home.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import Navigation from '../../Shared/Navigation/Navigation';
+
+const Home = () => {
+    return (
+        <div>
+            <Navigation></Navigation>
+            <h2>hello home</h2>
+        </div>
+    );
+};
+
+export default Home;
---
+++ src/Pages/Shared/Navigation/Navigation.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const Navigation = () => {
+    return (
+        <div>
+            <h2>23</h2>
+        </div>
+    );
+};
+
+export default Navigation;
